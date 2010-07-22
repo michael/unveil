99ba466e2d9c1f26fdae88e3226bea63f7fad25c
michael <ma@zive.at>
2010-07-23 00:20:37 +0200

    Introduced 'start' and 'finish' callbacks on Tweens. The constructor got smarter and now takes a more convenient options hash.

--- src/scene/tween.js
+++ src/scene/tween.js
@@ -35,18 +35,22 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 *****************************************/
 
-uv.Tween = function(obj, prop, func, begin, finish, duration) {
-  if (!arguments.length) return;
-  this.obj = obj;
-  this.prop = prop;
-  this.begin = begin;
-  this._pos = begin;
-  this.setDuration(duration);
-  if (func!=null && func!='') {
-    this.func = func;
-  }
-  this.setFinish(finish);
-}
+// uv.Tween = function(obj, prop, func, begin, finish, duration) {
+uv.Tween = function(opts) {
+  this.prop = opts.property;
+  this.obj = opts.obj;
+  this.begin = opts.begin || opts.obj[this.prop];
+  this._pos = this.begin;
+  this.setDuration(opts.duration);
+  
+  this.func = opts.easer || uv.Tween.strongEaseInOut;
+  this.setFinish(opts.finish || opts.obj[this.prop]);
+  
+  // callbacks
+  this.callbacks = {};
+  this.callbacks.start = function() {  };
+  this.callbacks.finish = function() {  };
+};
 
 uv.Tween.prototype = {
   obj: new Object(),
@@ -65,6 +69,9 @@ uv.Tween.prototype = {
   _finish: 0,
   name: '',
   suffixe: '',
+  on: function(name, fn) {
+    this.callbacks[name] = fn;
+  },
   setTime: function(t) {
   	this.prevTime = this._time;
   	if (t > this.getDuration()) {
@@ -75,8 +82,8 @@ uv.Tween.prototype = {
   		} else {
   			this._time = this._duration;
   			this.update();
-  			this.stop();
-  			// execute onFinished callback
+        
+        this.stop(); // CHECK!
   		}
   	} else if (t < 0) {
   		this.rewind();
@@ -132,10 +139,10 @@ uv.Tween.prototype = {
   start: function() {
   	this.rewind();
   	this._playing = true;
-  	// execute onStarted callback
+  	this.callbacks.start();
   },
   rewind: function(t) {
-  	this.stop();
+  	this.reset();
   	this._time = (t == undefined) ? 0 : t;
   	this.fixTime();
   	this.update();
@@ -156,9 +163,12 @@ uv.Tween.prototype = {
   nextFrame: function() {
   	this.setTime((this.getTimer() - this._startTime) / 1000);
   },
-  stop: function() {
+  reset: function() {
     this._playing = false;
-    // execute onStopped callback
+  },
+  stop: function() {
+    this._playing = false;    
+    this.callbacks.finish();
   },
   continueTo: function(finish, duration) {
   	this.begin = this._pos;
@@ -184,22 +194,22 @@ uv.Tween.prototype = {
   }
 };
 
-// Tweening functions
+// Easing functions
 uv.Tween.backEaseIn = function(t,b,c,d,a,p) {
 	if (s == undefined) var s = 1.70158;
 	return c*(t/=d)*t*((s+1)*t - s) + b;
 }
 
 uv.Tween.backEaseOut = function(t,b,c,d,a,p) {
-	if (s == undefined) var s = 1.70158;
+	if (s === undefined) var s = 1.70158;
 	return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
-}
+};
 
 uv.Tween.backEaseInOut = function(t,b,c,d,a,p) {
 	if (s == undefined) var s = 1.70158; 
 	if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
 	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-}
+};
 
 uv.Tween.elasticEaseIn = function(t,b,c,d,a,p) {
 	if (t==0) return b;  
@@ -212,14 +222,14 @@ uv.Tween.elasticEaseIn = function(t,b,c,d,a,p) {
 		var s = p/(2*Math.PI) * Math.asin (c/a);
 
 	return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
-}
+};
 
 uv.Tween.elasticEaseOut = function (t,b,c,d,a,p) {
 		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
 		if (!a || a < Math.abs(c)) { a=c; var s=p/4; }
 		else var s = p/(2*Math.PI) * Math.asin (c/a);
 		return (a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b);
-}
+};
 
 uv.Tween.elasticEaseInOut = function (t,b,c,d,a,p) {
 	if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) var p=d*(.3*1.5);
@@ -227,7 +237,7 @@ uv.Tween.elasticEaseInOut = function (t,b,c,d,a,p) {
 	else var s = p/(2*Math.PI) * Math.asin (c/a);
 	if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
 	return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
-}
+};
 
 uv.Tween.bounceEaseOut = function(t,b,c,d) {
 	if ((t/=d) < (1/2.75)) {
@@ -239,43 +249,43 @@ uv.Tween.bounceEaseOut = function(t,b,c,d) {
 	} else {
 		return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
 	}
-}
+};
 
 uv.Tween.bounceEaseIn = function(t,b,c,d) {
 	return c - Tween.bounceEaseOut (d-t, 0, c, d) + b;
-}
+};
 
 uv.Tween.bounceEaseInOut = function(t,b,c,d) {
 	if (t < d/2) return Tween.bounceEaseIn (t*2, 0, c, d) * .5 + b;
 	else return Tween.bounceEaseOut (t*2-d, 0, c, d) * .5 + c*.5 + b;
-}
+};
 
 uv.Tween.strongEaseInOut = function(t,b,c,d) {
 	return c*(t/=d)*t*t*t*t + b;
-}
+};
 
 uv.Tween.regularEaseIn = function(t,b,c,d) {
 	return c*(t/=d)*t + b;
-}
+};
 
 uv.Tween.regularEaseOut = function(t,b,c,d) {
 	return -c *(t/=d)*(t-2) + b;
-}
+};
 
 uv.Tween.regularEaseInOut = function(t,b,c,d) {
 	if ((t/=d/2) < 1) return c/2*t*t + b;
 	return -c/2 * ((--t)*(t-2) - 1) + b;
-}
+};
 
 uv.Tween.strongEaseIn = function(t,b,c,d) {
 	return c*(t/=d)*t*t*t*t + b;
-}
+};
 
 uv.Tween.strongEaseOut = function(t,b,c,d) {
 	return c*((t=t/d-1)*t*t*t*t + 1) + b;
-}
+};
 
 uv.Tween.strongEaseInOut = function(t,b,c,d) {
 	if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
 	return c/2*((t-=2)*t*t*t*t + 2) + b;
-}
+};
